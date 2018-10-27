dc102486b080e32d06181c4469a93dc2d4d5f05d
Magesh <magesh@hash14.com>
2018-10-28 00:27:15 +0530

    created the action to fetch data

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const API_KEY = "276eece1d8ec66b02abe6ad4e19aa44d";
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
